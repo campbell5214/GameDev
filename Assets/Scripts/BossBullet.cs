e0c689f8bf202a9501e63a7a9cade4e2ad5d54b8
Evan Phillips <ejp468@gmail.com>
2023-12-11 00:11:04 -0500

    bossfix?

--- Assets/Scripts/BossBullet.cs
+++ Assets/Scripts/BossBullet.cs
@@ -32,7 +32,7 @@ public class BossBullet : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if (timer > 7)
+        if (timer > 20)
         {
             Destroy(gameObject);
         }
